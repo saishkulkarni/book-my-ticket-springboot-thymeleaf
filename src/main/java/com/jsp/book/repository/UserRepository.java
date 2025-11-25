6d4344315643e88697bd0bbf47d042fbfd3429e3
saishkulkarni <saishkulkarni7@gmail.com>
2025-11-25 12:15:25 +0530

    Created Packages and Some Classes

---
+++ src/main/java/com/jsp/book/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jsp.book.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.book.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
